949cba2c9265a0c84b8438becc186ab9966e2f12
huangjinhui <279714010@qq.com>
2024-10-09 23:40:49 +0800

    配置补充

--- config/appearance.lua
+++ config/appearance.lua
@@ -3,57 +3,60 @@ local gpu_adapters = require('utils.gpu_adapter')
 local colors = require('colors.custom')
 
 return {
-   animation_fps = 60,
-   max_fps = 60,
-   -- front_end = 'WebGpu',
-   -- webgpu_power_preference = 'HighPerformance',
-   -- webgpu_preferred_adapter = gpu_adapters:pick_best(),
+	animation_fps = 60,
+	max_fps = 60,
+	-- front_end = 'WebGpu',
+	-- webgpu_power_preference = 'HighPerformance',
+	-- webgpu_preferred_adapter = gpu_adapters:pick_best(),
 
-   -- color scheme
-   colors = colors,
+	-- color scheme
+	colors = colors,
 
-   -- background
-   background = {
-      {
-         source = { File = wezterm.GLOBAL.background },
-         horizontal_align = 'Center',
-      },
-      {
-         source = { Color = colors.background },
-         height = '120%',
-         width = '120%',
-         vertical_offset = '-10%',
-         horizontal_offset = '-10%',
-         opacity = 0.96,
-      },
-   },
+	-- background
+	background = {
+		{
+			source = { File = wezterm.GLOBAL.background },
+			horizontal_align = 'Center',
+		},
+		{
+			source = { Color = colors.background },
+			height = '120%',
+			width = '120%',
+			vertical_offset = '-10%',
+			horizontal_offset = '-10%',
+			opacity = 0.96,
+		},
+	},
 
-   -- scrollbar
-   enable_scroll_bar = true,
+	-- scrollbar
+	enable_scroll_bar = true,
 
-   -- tab bar
-   enable_tab_bar = true,
-   hide_tab_bar_if_only_one_tab = false,
-   use_fancy_tab_bar = false,
-   tab_max_width = 25,
-   show_tab_index_in_tab_bar = false,
-   switch_to_last_active_tab_when_closing_tab = true,
+	-- tab bar
+	-- enable_tab_bar = true,
+	hide_tab_bar_if_only_one_tab = true,
+	use_fancy_tab_bar = false,
+	window_decorations = "RESIZE",
+	tab_max_width = 25,
+	show_tab_index_in_tab_bar = false,
+	switch_to_last_active_tab_when_closing_tab = true,
+	show_new_tab_button_in_tab_bar = false,
+	adjust_window_size_when_changing_font_size = true,
 
-   -- window
-   window_padding = {
-      left = 0,
-      right = 0,
-      top = 10,
-      bottom = 7.5,
-   },
-   window_close_confirmation = 'NeverPrompt',
-   window_frame = {
-      active_titlebar_bg = '#090909',
-      -- font = fonts.font,
-      -- font_size = fonts.font_size,
-   },
-   inactive_pane_hsb = {
-      saturation = 0.9,
-      brightness = 0.65,
-   },
+	-- window
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 10,
+		bottom = 7.5,
+	},
+	window_close_confirmation = 'NeverPrompt',
+	window_frame = {
+		active_titlebar_bg = '#090909',
+		-- font = fonts.font,
+		-- font_size = fonts.font_size,
+	},
+	inactive_pane_hsb = {
+		saturation = 0.9,
+		brightness = 0.65,
+	},
 }
