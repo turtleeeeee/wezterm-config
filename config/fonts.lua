90ec652e010d2c51913fa08fec0c326eec249d3e
huangjinhui <279714010@qq.com>
2024-10-09 14:59:31 +0800

    bugfix

--- config/fonts.lua
+++ config/fonts.lua
@@ -3,7 +3,7 @@ local platform = require('utils.platform')
 
 -- local font = 'Maple Mono SC NF'
 local font_family = 'JetBrainsMono Nerd Font Mono'
-local font_size = platform.is_mac and 16 or 12 
+local font_size = platform.is_mac and 16 or 12
 
 return {
    font = wezterm.font({
